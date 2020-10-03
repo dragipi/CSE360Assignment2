f247a5870d40bbc5670979b2904668664d299917
dragipi <dragipi@asu.edu>
2020-10-03 13:57:35 -0700

    Initial commit

---
+++ src/AddingMachine.java
@@ -0,0 +1,42 @@
+public class AddingMachine {
+    private int total;
+    /* initializes total to 0
+     *@param none
+     *@return nothing
+     */
+    public AddingMachine () {
+        total = 0;  // not needed - included for clarity
+    }
+    /* returns 0
+     *@param none
+     *@return 0
+     */
+    public int getTotal () {
+        return 0;
+    }
+    /* adds number to total
+     *@param value:Int
+     *@return nothing
+     */
+    public void add (int value) {
+    }
+    /* subtracts number from total
+     *@param value:Int
+     *@return nothing
+     */
+    public void subtract (int value) {
+    }
+    /* prints ""
+     *@param none
+     *@return ""
+     */
+    public String toString () {
+        return "";
+    }
+    /* clears total
+     *@param none
+     *@return none
+     */
+    public void clear() {
+    }
+}
